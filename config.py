3fbec760f380cba7ec951d708bb830eba6a6cab5
Khairul Imam <ki65559@gmail.com>
2018-10-27 11:50:25 +0800

    add base structure

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+DEBUG = True
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+THREADS_PER_PAGE = 2
+
+APP_NAME = 'Lombok Dev - Top 10 Hacktoberfest'
+
+SECRET_KEY = "cBWegL8d367vPzTp9Y2pJudLLtaKi5Jtw8//WsRjZrc="
